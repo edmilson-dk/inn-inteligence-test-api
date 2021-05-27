c3c63c7072c605fc3a0c230d8fdcdfb405c98980
edmilson-dk <droidvidaboa@gmail.com>
2021-05-27 18:44:03 -0300

    refactor: added new method type

--- src/domain/use-cases/movie-search/index.ts
+++ src/domain/use-cases/movie-search/index.ts
@@ -1,5 +1,6 @@
-import { MovieSearchDataPreviewDTO } from "src/domain/dtos/movie-search-dtos";
+import { MovieSearchDataOneMovieDTO, MovieSearchDataPreviewDTO } from "src/domain/dtos/movie-dtos";
 
 export interface IMovieSearchUseCases {
   getAllMoviesPreview: (title: string, movieType: string, page: number) => Promise<MovieSearchDataPreviewDTO[] | []>;
+  getOneMovieById: (id: string) => Promise<MovieSearchDataOneMovieDTO | []>;
 }
