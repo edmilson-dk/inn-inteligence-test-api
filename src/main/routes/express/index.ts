e7220d4c81e41cb0158a808d0a6afad2673ae9aa
edmilson-dk <droidvidaboa@gmail.com>
2021-05-27 15:17:56 -0300

    feat: created search all movies router

--- src/main/routes/express/index.ts
+++ src/main/routes/express/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { adaptRoute } from "../../adapters/express-adapter";
+import { makeGetAllMoviesFactory } from "src/main/factories/movie-search/get-all-movies-factory";
 
 const routes = Router();
 
-routes.get("/test", (_, res) => res.send({ ok: true }));
+routes.get("/search/all", adaptRoute(makeGetAllMoviesFactory()));
 
 export default routes;
