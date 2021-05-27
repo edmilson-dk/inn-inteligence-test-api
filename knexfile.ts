0d996fdf3db0fee70e574bdd64b4a36bd0f94e4e
edmilson-dk <droidvidaboa@gmail.com>
2021-05-26 23:45:14 -0300

    other: created knexfile config file

---
+++ knexfile.ts
@@ -0,0 +1,55 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
+import pg from 'pg';
+
+pg.defaults.ssl = {
+  rejectUnauthorized: false,
+}
+
+export default {
+  development: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      database: process.env.DATABASE,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: process.env.MIGRATIONS,
+      tableName: "knex_migrations"
+    }
+  },
+  test: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      database: "db_tests",
+      user: "usertests",
+      password: "tests123",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: process.env.MIGRATIONS,
+      tableName: "knex_migrations"
+    }
+  },
+  production: {
+    client: process.env.DB_CLIENT,
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: process.env.MIGRATIONS,
+      tableName: "knex_migrations"
+    }
+  }
+};
