87b7ce88b3b1418c21b7e77e28c2b781492ef69e
edmilson-dk <droidvidaboa@gmail.com>
2021-05-27 18:42:50 -0300

    refactor: added new method type

--- src/application/repositories/movie-api/index.ts
+++ src/application/repositories/movie-api/index.ts
@@ -1,5 +1,6 @@
-import { MovieSearchDataPreviewDTO } from "src/domain/dtos/movie-search-dtos";
+import { MovieSearchDataOneMovieDTO, MovieSearchDataPreviewDTO } from "src/domain/dtos/movie-dtos";
 
 export interface IMovieApiRepository {
   getAllMoviesPreview: (title: string, movieType: string, page: number) => Promise<MovieSearchDataPreviewDTO[] | null>;
+  getOneMovieById: (id: string) => Promise<MovieSearchDataOneMovieDTO | null>;
 }
